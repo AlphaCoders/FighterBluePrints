949ec20d101caf03326e5d62fd0164f81edeccb8
Sathish Phani Kurella <sathishphanikurella@gmail.com>
2016-03-23 15:16:35 +0530

    Create World.cs

---
+++ World.cs
@@ -0,0 +1,30 @@
+
+public class World
+{
+    #region Init
+    
+    public int[,] grid ;    // World's Grid 
+    public int gtime ;      // Game Time
+    
+    #endregion 
+    
+    #region Constructors
+    
+    public World()
+    {
+        grid = new int[500,500] ;
+        gtime = 0 ;
+    }
+    
+    #endregion
+    
+    #region Update
+    
+    public void Update()
+    {
+        // Here we make the movement and attacks and health deductions
+        gtime++ ;
+    }
+    
+    #endregion
+}
