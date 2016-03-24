3ddc6aacd5586330c987a79af41c3d6a38acb333
Sathish Phani Kurella <sathishphanikurella@gmail.com>
2016-03-24 11:58:25 +0530

    Update Begin.cs

--- Begin.cs
+++ Begin.cs
@@ -7,7 +7,11 @@ public class Begin
     public int fps ;                             // tmp Frame Number
     public int maxFps ;                          // Max FPS
     public int fastFactor ;                      // Ex: x 2 times the original speed
-    public Dictionary<int,Fighter> Units         // Holds ( id , Fighter ) Relationship
+    public Dictionary<int,Fighter> allUnits      // Holds ( id , Fighter ) Relationship
+    public int groundWidth ;                     // Battle Ground Width
+    public int groundLength ;                    // Battle Ground Length
+    public int grid[,,] ;                        // 3D Maxtrix ...(x,y,0) = id at x,y and (x,y,1) = gtime at x,y
+    public int arrows[,,] ;                      // Same as grid but (x,y,0) = Flag(0/1) and (x,y,1) = gtime
     
     #endregion 
     
@@ -15,7 +19,12 @@ public class Begin
     
     public Begin()
     {
-        Units = new Dictionary<int,Fighter>();
+        allUnits = new Dictionary<int,Fighter>();
+        groundWidth = 700 ;
+        groundLength = 1200 ;
+        grid = new int[groundLength,groundWidth,2] ;
+        arrows = new int[groundLength,groundWidth,2] ;
+        
         // Here we must add our capital
         // Also add enemy capitals as Static Units
         gtime = 0 ;
