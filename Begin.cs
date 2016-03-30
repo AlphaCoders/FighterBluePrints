d32b1a7ba80f4238f44cb2d5939193c01bdd2b19
Sathish Phani Kurella <sathishphanikurella@gmail.com>
2016-03-30 15:12:29 +0530

    Update Begin.cs

--- Begin.cs
+++ Begin.cs
@@ -7,13 +7,59 @@ public class Begin
     public int fps ;                             // tmp Frame Number
     public int maxFps ;                          // Max FPS
     public int fastFactor ;                      // Ex: x 2 times the original speed
-    public Dictionary<int,Fighter> allUnits      // Holds ( id , Fighter ) Relationship
+    public Dictionary<int,Fighter> allUnits ;    // Holds ( id , Fighter ) Relationship
     public int groundWidth ;                     // Battle Ground Width
     public int groundLength ;                    // Battle Ground Length
-    public int grid[,,] ;                        // 3D Maxtrix ...(x,y,0) = id at x,y and (x,y,1) = gtime at x,y
-    public int arrows[,,] ;                      // Same as grid but (x,y,0) = Flag(0/1) and (x,y,1) = gtime
+    public int[,,] grid ;                        // 3D Maxtrix ...(x,y,0) = id at x,y and (x,y,1) = gtime at x,y
+    public int[,,] arrows ;                      // Same as grid but (x,y,0) = Flag(0/1) and (x,y,1) = gtime
     
     #endregion 
+    
+    #region Testing Data
+    
+    public String board ;
+    
+    public int UnixTime()
+	{
+    	DateTime epochStart=new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    	return (int)(DateTime.UtcNow - epochStart).Ticks*100 ;
+	}
+    
+    public int Test_generateid( int _pid )
+    {
+    	Random r = new Random( UnixTime() );
+    	return r.Next( 1111 , 9999 );
+    }
+    
+    public void Test_populategrid( String _board )
+    {
+    	for ( int i = 0 ; i < groundLength ; i++ )
+    	{
+    		for ( int j = 0 ; j < groundWidth ; j++ )
+    		{
+    			char c =  _board[ ( i * groundWidth ) + j ] ;
+    			switch ( c )
+    			{
+					case 'p' :
+    					grid[i,j,0] = Test_generateid(1) ;
+    					// create Fighter with id and add to all units 
+    					grid[i,j,1] = gtime ;
+    					break ; 
+					case 'e' :
+						grid[i,j,0] = Test_generateid(2) ;
+						
+    					grid[i,j,1] = gtime ;
+    					break ; 
+					default :
+						grid[i,j,0] = 0 ;
+						grid[1,j,1] = gtime ;
+						break ;
+    			}
+    		}
+    	}
+    }
+    
+    #endregion
     
     #region Constructors
     
@@ -25,14 +71,25 @@ public class Begin
         fastFactor = 1 ;
         
         allUnits = new Dictionary<int,Fighter>();
-        groundWidth = 700 ;
-        groundLength = 1200 ;
+        groundWidth = 15 ;
+        groundLength = 7 ;
         grid = new int[groundLength,groundWidth,2] ;
         arrows = new int[groundLength,groundWidth,2] ;
         
         // Here we must add our capital
         // Also add enemy capitals as Static Units
-      
+        
+        board = 
+        "..............." +
+        "..........e...." +
+        "..............." +
+        "..............." +
+        "..............." +
+        "..p............" +
+        "..............." 
+        ;
+        
+      	Test_populategrid( board );
     }
     
     #endregion
