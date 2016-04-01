4bb171fb6b5a5fc051d7bb5c1547d3fe9813730b
Sathish Phani Kurella <sathishphanikurella@gmail.com>
2016-04-01 16:31:58 +0530

    Update Fighter.cs

--- Fighter.cs
+++ Fighter.cs
@@ -1,4 +1,3 @@
-
 public class Fighter
 {
 	
@@ -8,9 +7,11 @@ public class Fighter
     public int gid ;         // Group ID 
     public int wid ;         // Weapon ID
     public int pid ;         // Player ID
+    public int tid ;         // Team ID
     public int x ;           // Fighters X Cor
     public int y ;           // Fighters Y Cor
     public int z ;           // Fighters Z Cor
+    public int range  ;      // Fighters Sight Range
     public int health ;      // Health or Hitpoints
     public int attack ;      // Weapon Attack
     public int handattack ;  // Hand Attack
@@ -30,26 +31,35 @@ public class Fighter
     public int ex ;          // enemy X Cor
     public int ey ;          // enemy Y Cor
     public int ez ;          // enemy Z Cor
-    public int teid ;        // temporary enemy in betwen fighter and enemy
-    public int tex ;         // temporary enemy X Cor
-    public int tey ;         // temporary enemy Y Cor
-    public int tez ;         // temporary enemy Z Cor
+    
+
     
     #endregion
     
     #region Constructors
     
-    public Fighter( int _id , int _gid , int _wid , int _x , int _y , int _z )
+    public Fighter( int _id , int _gid , int _wid , int _pid , int _x , int _y , int _z )
     {
 		id = _id ;
 		gid = _gid ;
 		wid = _wid ;
+		pid = _pid ;
 		x = _x ;
 		y = _y ;
 		z = _z ;
 		mode = 0 ;
 		angle = 0 ;
 		giveWeapon( _wid );
+		eid = -1 ; 
+		egid = -1 ;
+		ex = -1 ;
+		ey = -1 ;
+		ez = -1 ;
+		tid = 1 ; // By default Team1
+		if ( _pid % 2 == 0 )
+		{
+			tid = 2 ; // All even pids to Team2
+		}
     }
     
     #endregion
@@ -59,6 +69,7 @@ public class Fighter
     private void giveWeapon( int _wid )
     {
     	Constants c = new Constants( _wid );
+    	range = c.getRange();
     	health = c.getHealth();
     	attack = c.getAttack();
     	handattack = c.getHandattack();
@@ -68,14 +79,26 @@ public class Fighter
     	delay = c.getDelay();
     }
     
+   
+    
+    public override String ToString()
+    {
+    	return String.Format(
+    		"     ID : {0} \t " +
+        //  "    GID : {1} \t " +
+    		"    WID : {2} \t " +
+    		"    PID : {3} \t " +
+    		"      X : {4} \t " +
+    		"      Y : {5} \t " +
+    	//  "      Z : {6} \t " +
+    		" HEALTH : {7} \t " +
+    		" ATTACK : {8} \t " +
+    	"",id,gid,wid,pid,x,y,z,health,attack);
+    }
+    
     #endregion
     
 }
-
-
-
-
-
 
 
 
